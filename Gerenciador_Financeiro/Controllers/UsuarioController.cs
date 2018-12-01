c2a0eb72c447c47aea6012f737c2db227dff9127
alcyonjr <alcyon.rotter.junior@gmail.com>
2018-11-30 22:11:31 -0200

    Criação dos controllers da aplicação

---
+++ Gerenciador_Financeiro/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Gerenciador_Financeiro.Controllers
+{
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {        
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+     
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+        
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
