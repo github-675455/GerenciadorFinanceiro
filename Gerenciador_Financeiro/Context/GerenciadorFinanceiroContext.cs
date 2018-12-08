fceb7ace2590a6665ca151398d918ffe0008a159
github-675455 <github-675455@vinicius.sl>
2018-12-07 22:05:08 -0200

    Final implementation of crud, configured with mysql

--- Gerenciador_Financeiro/Context/GerenciadorFinanceiroContext.cs
+++ Gerenciador_Financeiro/Context/GerenciadorFinanceiroContext.cs
@@ -6,6 +6,9 @@ namespace Gerenciador_Financeiro.Context
 {
     public class GerenciadorFinanceiroContext : DbContext
     {
+        public GerenciadorFinanceiroContext(DbContextOptions options) : base(options)
+        {
+        }
         public DbSet<Conta> Contas { get; set; }
         public DbSet<Despesa> Despesas { get; set; }
         public DbSet<Receita> Receitas { get; set; }
