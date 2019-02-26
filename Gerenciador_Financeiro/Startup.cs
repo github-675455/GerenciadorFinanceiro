b779f5007735d43947d00e35590bd87a6bf555bb
github-675455 <github-675455@vinicius.sl>
2019-02-26 00:47:13 -0300

    Did'nt compile, forgot to build locally.

--- Gerenciador_Financeiro/Startup.cs
+++ Gerenciador_Financeiro/Startup.cs
@@ -31,13 +31,7 @@ namespace Gerenciador_Financeiro
         {
             Configuration = configuration;
         }
-
-        public Startup(IConfiguration configuration) 
-        {
-            this.Configuration = configuration;
-               
-        }
-                public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
